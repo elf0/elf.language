bc122c580cf155f8ecc00a3c5c1b9bd388c3172b
elf <elf198012@gmail.com>
2016-08-06 01:29:51 +0800

    Use KVs format for source file. And use ':' for indent. It's simple and poweful! "KVReader.h" can read it.

--- compiler/Type.h
+++ compiler/Type.h
@@ -3,10 +3,20 @@
 
 //License: Public Domain
 //Author: elf
-//EMail: elf198012@gmail.com
+//EMail: elf@elf0.org
 
-typedef unsigned char      Byte;
-typedef unsigned char      Char;
+#if defined(__GNUC__) \
+    && defined(__BYTE_ORDER__)
+
+#if defined(__ORDER_BIG_ENDIAN__) \
+    && (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+#define ELF_BIG_ENDIAN
+#elif defined(__ORDER_LITTLE_ENDIAN__) \
+    && (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+#define ELF_LITTLE_ENDIAN
+#endif
+
+#endif
 
 typedef unsigned char      U8;
 typedef unsigned short     U16;
@@ -21,25 +31,46 @@ typedef signed long long  I64;
 typedef float              F32;
 typedef double             F64;
 
-typedef U8 Bool;
+#if defined(__x86_64__) || defined(_WIN64)
+typedef U64 U;
+typedef I64 I;
+typedef F64 F;
+#else
+typedef U32 U;
+typedef I32 I;
+typedef F32 F;
+#endif
+
+//Character
+typedef U8 C;
+typedef U16 C16;
+typedef U32 C32;
+
+//Boolean
+typedef U8 B;
 #define false 0
 #define true  1
 
-typedef Byte* Pointer;
+typedef U8 Byte;
+
+typedef Byte* P;
 #define null  0
 
-typedef signed long Result;
+//Error code
+typedef U8 E8;
+typedef U16 E16;
+typedef U32 E32;
+typedef U64 E64;
+typedef U E;
 
-//namespace elf{
-//class Type{
-//public:
-//    enum class Type{
-//        ttBoolean,
-//        ttUnsigned8, ttUnsigned16, ttUnsigned32, ttUnsigned64,
-//        ttInteger8, ttInteger16, ttInteger32, ttInteger64,
-//        ttFloat32, ttFloat64
-//    };
-//};
-//}//namespace elf
+//Result code
+typedef I8 R8;
+typedef I16 R16;
+typedef I32 R32;
+typedef I64 R64;
+typedef I R;
+
+#define ContainerOf(pointer, Member, Container) \
+    ((Container*)((P)(pointer) - (P)&((Container*)0)->Member))
 
 #endif //TYPE_H
